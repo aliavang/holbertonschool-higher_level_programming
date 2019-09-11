f2f33a9a60920d4c3337d4bf4f33236b221d849c
aliavang <714@holbertonschool.com>
2019-09-11 19:55:37 +0000

    First commit, task 13 and task 13 header file

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * insert_node - Insert number into sorted linked list
+ * @head: Double pointer to head
+ * @number: Number to place into node
+ *
+ * Return: Address of new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *tmp;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = number;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		if (tmp->n <= number && tmp->next->n >= number)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			break;
+		}
+		tmp = tmp->next;
+	}
+	return (new);
+}
