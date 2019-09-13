325c8d1d2a4478f7517105b36796867d3d9b3f8d
aliavang <714@holbertonschool.com>
2019-09-13 03:26:39 +0000

    First commit, task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+args = len(argv)
+if args > 0:
+    if args == 2:
+        print("{:d} argument:\n1: {:s}".format(args - 1, argv[1]))
+    else:
+        print("{:d} arguments".format(args - 1))
+        i = 1
+        while i < args:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
+else:
+    print("0 arguments.")
