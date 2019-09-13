c956b612a8ed5be382216d923c631064cfc0fb36
aliavang <714@holbertonschool.com>
2019-09-13 08:09:20 +0000

    Added condition to check for legal operators, if not exit status 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     args = len(argv) - 1
     if not args == 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    if argv[2] != "+" or argv[2] != "-" or argv[2] != "*" or argv[2] != "/":
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
     else:
         a = int(argv[1])
         b = int(argv[3])
