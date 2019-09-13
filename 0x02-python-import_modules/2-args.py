44f64426d742be18352667726cde23ec405bd279
aliavang <714@holbertonschool.com>
2019-09-13 03:31:31 +0000

    Added ':' for more than 1 argument, line 8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if args > 0:
     if args == 2:
         print("{:d} argument:\n1: {:s}".format(args - 1, argv[1]))
     else:
-        print("{:d} arguments".format(args - 1))
+        print("{:d} arguments:".format(args - 1))
         i = 1
         while i < args:
             print("{:d}: {:s}".format(i, argv[i]))
