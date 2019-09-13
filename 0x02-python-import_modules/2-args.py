b7878be4cd4bbcc327ff68a91f1c0d84a1f92c43
aliavang <714@holbertonschool.com>
2019-09-13 03:33:37 +0000

    Included check to see if file is being executed as main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
-args = len(argv)
-if args > 0:
-    if args == 2:
-        print("{:d} argument:\n1: {:s}".format(args - 1, argv[1]))
+if __name__ == "__main__":
+    from sys import argv
+    args = len(argv)
+    if args > 0:
+        if args == 2:
+            print("{:d} argument:\n1: {:s}".format(args - 1, argv[1]))
+        else:
+            print("{:d} arguments:".format(args - 1))
+            i = 1
+            while i < args:
+                print("{:d}: {:s}".format(i, argv[i]))
+                i += 1
     else:
-        print("{:d} arguments:".format(args - 1))
-        i = 1
-        while i < args:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
-else:
-    print("0 arguments.")
+        print("0 arguments.")
