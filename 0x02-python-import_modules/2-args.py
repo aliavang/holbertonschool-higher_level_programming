c58210469c07eea281f5468bc81738590d40ab6a
aliavang <714@holbertonschool.com>
2019-09-13 03:41:02 +0000

    Changed while to for loop ln 10 and condition at ln 6 to 'args == 1'

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,11 @@ if __name__ == "__main__":
     from sys import argv
     args = len(argv) - 1
     if args > 0:
-        if args == 2:
+        if args == 1:
             print("{:d} argument:\n1: {:s}".format(args, argv[1]))
         else:
             print("{:d} arguments:".format(args))
-            i = 1
-            while i < args:
-                print("{:d}: {:s}".format(i, argv[i]))
-                i += 1
+            for i in range(args):
+                print("{:d}: {:s}".format(i + 1, argv[i + 1]))
     else:
         print("{:d} arguments.".format(args))
