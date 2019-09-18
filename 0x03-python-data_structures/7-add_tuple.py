58bb25c684e9d09b48373fc122170cc0bf2b3205
aliavang <714@holbertonschool.com>
2019-09-18 22:48:35 +0000

    First commit, task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a, len_b = len(list_a), len(list_b)
+    if len_a == 1:
+        tuple_a += (0, )
+    elif len_a == 0:
+        tuple_a += (0, 0)
+    elif len_b == 1:
+        tuple_b += (0, )
+    elif len_b == 0:
+        tuple_b += (0, 0)
+    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return tuple_c
