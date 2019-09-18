49404c6c26604fa1d8537e4203b50c55225141bf
aliavang <714@holbertonschool.com>
2019-09-18 23:29:36 +0000

    Removed if...elif logic and replaced with two if's

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a, tuple_b = tuple_a[:2], tuple_b[:2]
     len_a, len_b = len(tuple_a), len(tuple_b)
-    if len_a == 1:
-        tuple_a += (0, )
-    elif len_a == 0:
+    if len_a == 1 or len_a == 0:
         tuple_a += (0, 0)
-    elif len_b == 1:
-        tuple_b += (0, )
-    elif len_b == 0:
+    if len_b == 1 or len_b == 0:
         tuple_b += (0, 0)
-    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    tuple_a = tuple_a[0:2]
+    tuple_b = tuple_b[0:2]
+    print(tuple_a)
+    print(tuple_b)
+    tuple_c = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return tuple_c
