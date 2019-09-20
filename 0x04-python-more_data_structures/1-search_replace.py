eaf5ecb9944230a5087a77d09d76a0fe20e9dcf0
aliavang <714@holbertonschool.com>
2019-09-20 03:16:38 +0000

    Added loop to find all instances of search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    idx = new_list.index(search)
-    new_list[idx] = replace
+    for ele in new_list:
+        if ele == search:
+            idx = new_list.index(search)
+            new_list[idx] = replace
     return new_list
