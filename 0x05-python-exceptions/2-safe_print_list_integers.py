edeb217c4bb9e182dd2a3ac759715bd78e1bcca2
aliavang <714@holbertonschool.com>
2019-09-30 23:51:33 +0000

    First commit, task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    nb = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            nb += 1
+        except (TypeError, ValueError):
+            pass
+        i += 1
+    print("")
+    return nb
