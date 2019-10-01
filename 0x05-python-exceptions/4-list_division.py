2b2e33563177e10377476a9d1e6555387b9654d0
aliavang <714@holbertonschool.com>
2019-10-01 00:13:22 +0000

    First commit, task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    new_list = []
+    while i < list_length:
+        try:
+            quo = my_list_1[i] / my_list_2[i]
+            new_list.append(quo)
+        except TypeError:
+            new_list.append(0)
+            print("wrong type")
+        except ZeroDivisionError:
+            new_list.append(0)
+            print("division by 0")
+        except IndexError:
+            new_list.append(0)
+            print("out of range")
+        finally:
+            pass
+        i += 1
+    return new_list
