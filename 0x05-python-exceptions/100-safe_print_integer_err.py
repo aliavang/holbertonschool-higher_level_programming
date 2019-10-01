7196aeb82e1fd2fe6e1b2e7b584b776af3c9ee3e
aliavang <714@holbertonschool.com>
2019-10-01 02:43:29 +0000

    Removed string and changed to casting err as string; ln 8

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as err:
-        print("Exception: Unknown format code 'd' for object of type 'str'",
-              file=sys.stderr)
+        print("Exception: " + str(err), file=sys.stderr)
         return False
