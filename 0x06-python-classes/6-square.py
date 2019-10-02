665507d551de9eca1b5e70ae882eceb10ba35c90
aliavang <714@holbertonschool.com>
2019-10-02 18:28:50 +0000

    Changed 'and' to 'or'; ln 39

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,7 +36,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if not type(value[0]) == int and not type(value[1]) == int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 and value[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
