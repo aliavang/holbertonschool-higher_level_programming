69d8247df6391fa4aff396da28b08881dd34770f
aliavang <714@holbertonschool.com>
2019-10-02 06:41:08 +0000

    Readded backslash to end of ln 48, changed '__size' to 'size'; ln 7

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize fields of class Square"""
-        self.__size = size
+        self.size = size
         self.__position = position
 
     def area(self):
@@ -45,8 +45,8 @@ class Square:
     @position.setter
     def position(self, value):
         """Set position of square"""
-        if not type(value) == int and not len(value) == 2
-        and value[0] < 0 and value[1] < 0:
+        if not type(value) == int and not len(value) == 2\
+           and value[0] < 0 and value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value[0:2]
