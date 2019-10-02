b465ebd0cf1711a545eb87bf92ad6d3cdfe71647
aliavang <714@holbertonschool.com>
2019-10-02 06:33:29 +0000

    First commit, task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+class Square:
+    """Create class Square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize fields of class Square"""
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """Find area of Square of x size"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Retrieve size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set value of size if valid value"""
+        if not type(value) == int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """Print square of x size"""
+        for y in range(self.__position[1]):
+            print("")
+        for r in range(self.__size):
+            for z in range(self.position[0]):
+                print(" ", end="")
+            for c in range(self.__size):
+                print("#", end="")
+            print("")
+
+    @property
+    def position(self):
+        """Retrieve position of square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set position of square"""
+        if not type(value) == int and not len(value) == 2\
+        and value[0] < 0 and value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value[0:2]
