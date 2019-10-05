46148c42b1f019dd1a5e21d36b4b1a083f9cbfed
aliavang <714@holbertonschool.com>
2019-10-05 04:31:27 +0000

    First commit, task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+def text_indentation(text):
+    """Print two new newline after a '.', '?', ':'
+
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    else:
+        c = 0
+        while c < len(text):
+            print("{:s}".format(text[c]), end="")
+            if text[c] == '.' or text[c] == '?' or text[c] == ':':
+                print("\n")
+                c += 1
+            c += 1
