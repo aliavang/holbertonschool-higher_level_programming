3cf24f1cd887938e6f057246bcdb2c9d9f63cf35
aliavang <714@holbertonschool.com>
2019-10-05 06:13:51 +0000

    Fixed documentation formatting

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,7 +6,6 @@ python3 -c 'print(__import__("my_module").__doc__)'
 
 def add_integer(a, b=98):
     """Return sum of a and b
-
     python3 -c 'print(__import__("my_module").my_function.__doc__)'
     """
 
