b01106b7f3db36de8b838bb9f037d45671ced0bd
aliavang <714@holbertonschool.com>
2019-10-05 04:30:25 +0000

    First commit, task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+def add_integer(a, b=98):
+    """Return sum of a and b
+
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a, b = int(a), int(b)
+    return a + b
