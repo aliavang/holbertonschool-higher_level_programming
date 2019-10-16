a615ce00c757a2f1e95d717a8a2847197d921f9f
aliavang <714@holbertonschool.com>
2019-10-16 05:41:12 +0000

    Changed isinstance to type; ln 24

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -21,7 +21,7 @@ class BaseGeometry:
         python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
-        if not isinstance(value, int):
+        if not type(value) == int:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
