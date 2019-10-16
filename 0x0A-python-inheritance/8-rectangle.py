1a05285fc40ca868542f835411cc7f319ebc4adb
aliavang <714@holbertonschool.com>
2019-10-16 05:41:25 +0000

    First commit, task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+class BaseGeometry:
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
+    def area(self):
+        """Return area of the geometry
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate value
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        if not type(value) == int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
+
+    def __init__(self, width, height):
+        """Initialize
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
