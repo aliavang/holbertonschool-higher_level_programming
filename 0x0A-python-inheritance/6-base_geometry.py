78b15f4ecdc10b5148a18a9789356f8bd5784035
aliavang <714@holbertonschool.com>
2019-10-16 05:07:34 +0000

    Correction of spelling for exception

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -13,4 +13,4 @@ class BaseGeometry:
         python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
-        raise Execption("area() is not implemented")
+        raise Exception("area() is not implemented")
