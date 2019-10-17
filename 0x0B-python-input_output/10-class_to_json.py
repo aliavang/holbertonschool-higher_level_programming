59dc6054fa7969a220de8e6dc245441beae2af27
aliavang <714@holbertonschool.com>
2019-10-17 04:08:35 +0000

    First commit, task 10

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+def class_to_json(obj):
+    """Return dictionary description for JSON serialized object
+
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    """
+    return obj.__dict__
