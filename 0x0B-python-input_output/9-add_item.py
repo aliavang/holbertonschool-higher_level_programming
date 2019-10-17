eb279b37cf1d238eb38ca0a3698cfc18864394b6
aliavang <714@holbertonschool.com>
2019-10-17 03:37:38 +0000

    First commit, task 9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+from sys import argv
+save = __import__('7-save_to_json_file').save_to_json_file
+load = __import__('8-load_from_json_file').load_from_json_file
+
+list = []
+l = len(argv)
+for arg in range(1, l):
+    list.append(argv[arg])
+save(list, "add_item.py")
+load("add_item.py")
