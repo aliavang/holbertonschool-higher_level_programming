1d0cc6dfe1b02b904687fc8b4a6d80c018042144
aliavang <714@holbertonschool.com>
2019-10-17 04:23:38 +0000

    First commit, task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+class Student:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieve dictionary representation of Student
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.\
+        __doc__)'
+        """
+        return self.__dict__
