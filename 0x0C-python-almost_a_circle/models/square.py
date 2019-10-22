d0a92c591a149b2aa58e264fab67ac82436a1a21
aliavang <714@holbertonschool.com>
2019-10-22 22:05:37 +0000

    Added getter and setter method, task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,22 @@ class Square(Rectangle):
         """
         return "[Square] ({}) {}/{} - {}".\
             format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Return value of size
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set value of size
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.width = value
+        self.height = value
