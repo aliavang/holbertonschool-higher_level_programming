683f3f72e9142a9521d0e0b4bc3a4703aac822a7
aliavang <714@holbertonschool.com>
2019-10-22 08:03:02 +0000

    First commit, task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+class Base:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize ID
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        if not id is None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
