25a8e8bd44eeced06cd905c876481af52ee37ae4
aliavang <714@holbertonschool.com>
2019-10-22 09:17:08 +0000

    First commit, task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+from models.base import Base
+class Rectangle(Base):
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize rectangle
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Return value of width
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set value of width
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """Return value of height
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set value of height
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """Return value of x
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set value of x
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """Return value of y
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set value of y
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.__y = value
