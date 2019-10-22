f4db5b7f6ce399ee5f1ffedd4ebe78c8ee465d5a
aliavang <714@holbertonschool.com>
2019-10-22 21:48:19 +0000

    First commit, task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize square
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        super().__init__(size, size, x, y)
+
+    def __str__(self):
+        """Return information on square in string format
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        return "[Square] ({}) {}/{} - {}".\
+            format(self.id, self.x, self.y, self.width)
