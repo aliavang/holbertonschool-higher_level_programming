26c5fbcf766fa8087c6381de4eb20fd2bfb69d00
aliavang <714@holbertonschool.com>
2019-10-23 04:05:18 +0000

    Added from_json_string method, task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,3 +32,28 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write JSON string to file
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        filename = "{}.json".format(cls)
+        my_obj = Base.to_json_string([list_objs])
+        with open(filename, 'w') as f:
+            json.dump(my_obj, f)
+
+    @classmethod
+    def from_json_string(json_string):
+        """Return list of JSON string representation
+
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        import json
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
