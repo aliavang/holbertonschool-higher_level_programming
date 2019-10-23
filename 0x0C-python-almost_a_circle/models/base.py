05d4e4e95bc2576329feeb02b033110fd0423269
aliavang <714@holbertonschool.com>
2019-10-23 03:10:56 +0000

    Removed double quotes around square brackets and added @staticmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,6 +20,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Return JSON string representation
 
@@ -27,7 +28,7 @@ class Base:
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
         import json
-        if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []
         else:
             return json.dumps(list_dictionaries)
