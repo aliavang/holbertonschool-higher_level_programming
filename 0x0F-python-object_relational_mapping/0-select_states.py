1201ad7852bd763d3524fc8848ddd9003c85b062
aliavang <714@holbertonschool.com>
2019-12-16 09:47:05 +0000

    First commit, task 0 and 1

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script to list all states from database
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    con = MySQLdb.connect(host="localhost",
+                          port=3306,
+                          user=sys.argv[1],
+                          passwd=sys.argv[2],
+                          db=sys.argv[3],
+                          charset="utf8")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    con.close()
