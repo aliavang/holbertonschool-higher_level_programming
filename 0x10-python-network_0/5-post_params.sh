1b4686b7965f46cac80fe3cd0db4a430000d1d8d
aliavang <714@holbertonschool.com>
2020-01-08 04:59:36 +0000

    Fixed for file to be 3 lines

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Send POST request and display body of response
-curl -sX "POST" -F "email=hr@holbertonschool.com"
--F "subject=I will always be here for PLD" $1
+curl -sX "POST" -F "email=hr@holbertonschool.com" -F "subject=I will always be here for PLD" $1
