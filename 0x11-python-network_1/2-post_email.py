4704f0dacadbf70a213d88977451d8a4d86269a9
aliavang <714@holbertonschool.com>
2020-02-01 01:17:22 +0000

    Changed code to post and not get

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,12 +3,16 @@
 Send URL and email address as POST request to URL and displays response
 """
 import urllib.request
+import urllib.parse
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    with urllib.request.urlopen(url) as response:
-        html = response.info()
-        print("Your email is: {}".format(html.get("email")))
+    email = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(email)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
