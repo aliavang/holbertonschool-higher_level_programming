b21c65bb40647fafad24b07f9503df917c5453ff
aliavang <714@holbertonschool.com>
2020-02-05 06:26:08 +0000

    First commit, task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Print three lines using loop
+const myStrings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+while (i < 3) {
+  console.log(myStrings[i]);
+  i++;
+}
