7b309f91c5115a8b2e14945ca2fba96a215680ec
aliavang <714@holbertonschool.com>
2020-02-05 20:09:26 +0000

    First commit, task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Send request to URL and display error code
+"""
+import urllib.request
+from urllib.error import HTTPError
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print("{}".format(html))
+    except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e))
